27f7c145e12ea0ea5c58abc283d18157779278e5
rame0 <r@me0.biz>
2023-07-31 01:34:38 +0600

    Поправил типы в `props`

--- UI/src/components/UI/BondOptionsRadios.vue
+++ UI/src/components/UI/BondOptionsRadios.vue
@@ -1,18 +1,21 @@
 <template>
   <el-radio-group v-model="value" size="small" @change="$emit('update:modelValue', $event)">
-    <el-radio-button v-for="option in options" :key="option.label" :label="option.value">
+    <el-radio-button v-for="option in options" :key="option.label as string" :label="option.value">
       {{ option.label }}
     </el-radio-button>
   </el-radio-group>
 </template>
 
 <script lang="ts">
+import type { PropType } from 'vue'
+import { type CollationItem } from '@/data/collations/BaseCollations'
+
 export default {
   name: 'BondOptionsRadios',
 
   props: {
     options: {
-      type: Array,
+      type: Array as PropType<CollationItem[]>,
       default: () => [
         { label: 'Не важно', value: -1 },
         { label: 'Да', value: true },
@@ -20,11 +23,10 @@ export default {
       ]
     },
     modelValue: {
-      type: [Boolean, String, Number, undefined],
+      type: Object as PropType<boolean | string | number | undefined>,
       default: undefined
     }
   },
-
 
   data() {
     return {
