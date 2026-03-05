9d6a2aef894ca9473a4ff3d8b08b2e5fc757a1e4
rame0 <r@me0.biz>
2026-03-06 02:24:59 +0300

    fix(ui): вынос директив Tailwind в отдельный CSS из-за порядка @use в Sass

--- UI/src/main.ts
+++ UI/src/main.ts
@@ -4,6 +4,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import '@/assets/tailwind.css'
 import '@/assets/index.scss'
 
 const pinia = createPinia()
